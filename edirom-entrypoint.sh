10cab84e461b272888354065da4942106c135c87
bwbohl <b.w.bohl@gmail.com>
2025-05-08 17:47:07 +0200

    Update edirom-entrypoint.sh
    Only execute cp if XARs found

--- edirom-entrypoint.sh
+++ edirom-entrypoint.sh
@@ -6,11 +6,10 @@ echo "# Welcome to docker-edirom-online #"
 echo "###################################"
 echo
 echo "This docker container is based on stadlerpeter/existdb:6."
-echo "In order to deploy your data XARs for Edirom-Online place"
-echo "them in ›add-xars‹ directory next to the Dockerfile."
 echo
-echo "copying additional XARs…"
-cp /var/add-xars/*.xar $EXIST_HOME/autodeploy/
+echo "checking for XARs at /var/add-xars/"
+xars=(/var/add-xars/*.xar)
+[ -f "$xars" ] && echo "copying additional XARs…" && cp /var/add-xars/*.xar $EXIST_HOME/autodeploy/ || { echo "no XARs found"; }
 echo
 # starting the original image's entrypoint
 echo "starting base-image…"
